e935000ca24fe43fd26033fc11a1094dd28cd59f
ExerciseMarch24 <mdmahr@metropolia.fi>
2024-03-24 01:26:21 +0200

    some changes

--- ex01.py
+++ ex01.py
@@ -1,4 +1,31 @@
-x = 2
+"""x = 2
 y = 3
+print(x+y)"""
 
-print(x+y)
+"""x = " awesome"
+def myfunc():
+    print("python is "+ x)
+myfunc()"""
+
+"""x = " awesome"
+def myfunc():
+    x = " fantastic"
+    print("python is" +x)
+myfunc()
+print("python is"+x)"""
+
+"""x = 22+5j
+print(type(x))"""
+
+"""import random
+print(random.randrange(1,15))"""
+
+"""txt = "The best thing is life are free"
+print("free" in txt)"""
+
+"""txt = "the best thing in life is free"
+if "free" in txt:
+    print("yes,'free' is present")"""
+
+
+
